40c89193893e83a6b32e0c29aeab29488e266bbf
biyanbahtiar <biyan.bahtiar@gmail.com>
2024-11-02 16:36:27 +0700

    Resolve conflict

--- postgres/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE absensi_table(
-    id SERIAL PRIMARY KEY,
-    nama VARCHAR(50) NOT NULL,
-    nim VARCHAR(50) NOT NULL,
-    mata_kuliah VARCHAR(100) NOT NULL,
-    jurusan VARCHAR(100) NOT NULL
-);
